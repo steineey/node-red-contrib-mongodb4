e2eba602a2bff6d03bb347fc90c01a50c1d405dd
steineey <christopher.steinbach@gmx.de>
2021-12-08 21:27:04 +0100

    prettier

--- mongodb4/mongodb4.js
+++ mongodb4/mongodb4.js
@@ -1,155 +1,155 @@
 module.exports = function (RED) {
-	var MongoClient = require("mongodb").MongoClient;
+  var MongoClient = require("mongodb").MongoClient;
 
-	function ClientNode(n) {
-		RED.nodes.createNode(this, n);
+  function ClientNode(n) {
+    RED.nodes.createNode(this, n);
 
-		// set database connection url
-		this.url = `${n.protocol}://${n.hostname}:${n.port}`;
-		this.dbName = n.dbName;
+    // set database connection url
+    this.url = `${n.protocol}://${n.hostname}:${n.port}`;
+    this.dbName = n.dbName;
 
-		// mongo client options
-		this.options = {};
-		if (this.credentials.username || this.credentials.password) {
-			this.options.auth = {
-				username: this.credentials.username,
-				password: this.credentials.password,
-			};
-			this.options.authSource = n.authSource;
-			this.options.authMechanism = n.authMechanism;
-		}
-		if (n.tls) this.options.tls = n.tls;
-		if (n.tlsCAFile) this.options.tlsCAFile = n.tlsCAFile;
-		if (n.tlsInsecure) this.options.tlsInsecure = n.tlsInsecure;
+    // mongo client options
+    this.options = {};
+    if (this.credentials.username || this.credentials.password) {
+      this.options.auth = {
+        username: this.credentials.username,
+        password: this.credentials.password,
+      };
+      this.options.authSource = n.authSource;
+      this.options.authMechanism = n.authMechanism;
+    }
+    if (n.tls) this.options.tls = n.tls;
+    if (n.tlsCAFile) this.options.tlsCAFile = n.tlsCAFile;
+    if (n.tlsInsecure) this.options.tlsInsecure = n.tlsInsecure;
 
-		this.client = null;
+    this.client = null;
 
-		var node = this;
+    var node = this;
 
-		node.connect = function () {
-			this.client = new MongoClient(this.url, this.options);
-			return this.client.connect();
-		};
-	}
+    node.connect = function () {
+      this.client = new MongoClient(this.url, this.options);
+      return this.client.connect();
+    };
+  }
 
-	RED.nodes.registerType("mongodb4-client", ClientNode, {
-		credentials: {
-			username: { type: "text" },
-			password: { type: "password" },
-		},
-	});
+  RED.nodes.registerType("mongodb4-client", ClientNode, {
+    credentials: {
+      username: { type: "text" },
+      password: { type: "password" },
+    },
+  });
 
-	function OperationNode(n) {
-		RED.nodes.createNode(this, n);
-		var node = this;
-		node.n = {
-			client: RED.nodes.getNode(n.clientNode),
-			connection: null,
-			database: null,
-			counter: {
-				success: 0,
-				error: 0,
-			},
-			collection: n.collection,
-			operation: n.operation,
-			output: n.output,
-		};
+  function OperationNode(n) {
+    RED.nodes.createNode(this, n);
+    var node = this;
+    node.n = {
+      client: RED.nodes.getNode(n.clientNode),
+      connection: null,
+      database: null,
+      counter: {
+        success: 0,
+        error: 0,
+      },
+      collection: n.collection,
+      operation: n.operation,
+      output: n.output,
+    };
 
-		var connect = async function () {
-			node.status({ fill: "yellow", shape: "ring", text: "connecting" });
+    var connect = async function () {
+      node.status({ fill: "yellow", shape: "ring", text: "connecting" });
 
-			try {
-				node.n.connection = await node.n.client.connect();
+      try {
+        node.n.connection = await node.n.client.connect();
 
-				// get database
-				node.n.database = node.n.connection.db(node.n.client.dbName);
-				await node.n.database.command({ ping: 1 });
-				node.status({ fill: "green", shape: "dot", text: "connected" });
+        // get database
+        node.n.database = node.n.connection.db(node.n.client.dbName);
+        await node.n.database.command({ ping: 1 });
+        node.status({ fill: "green", shape: "dot", text: "connected" });
 
-				node.on("input", async function (msg, send, done) {
-					send =
-						send ||
-						function () {
-							node.send.apply(node, arguments);
-						};
+        node.on("input", async function (msg, send, done) {
+          send =
+            send ||
+            function () {
+              node.send.apply(node, arguments);
+            };
 
-					try {
-						// get collection
-						var collection = msg.collection || node.n.collection;
-						var c = node.n.database.collection(collection);
+          try {
+            // get collection
+            var collection = msg.collection || node.n.collection;
+            var c = node.n.database.collection(collection);
 
-						// get operation
-						var operation = msg.operation || node.n.operation;
-						if (typeof c[operation] !== "function") {
-							throw `Operation "${operation}" is not supported by collection.`;
-						}
+            // get operation
+            var operation = msg.operation || node.n.operation;
+            if (typeof c[operation] !== "function") {
+              throw `Operation "${operation}" is not supported by collection.`;
+            }
 
-						// execute request
-						var request = c[operation](...msg.payload);
+            // execute request
+            var request = c[operation](...msg.payload);
 
-						// continue with response
-						if (operation === "aggregate" || operation === "find") {
-							switch (node.n.output) {
-								case "toArray":
-									msg.payload = await request.toArray();
-									send(msg);
-									break;
+            // continue with response
+            if (operation === "aggregate" || operation === "find") {
+              switch (node.n.output) {
+                case "toArray":
+                  msg.payload = await request.toArray();
+                  send(msg);
+                  break;
 
-								case "forEach":
-									await request.forEach(function (payload) {
-										msg.payload = payload;
-										send(msg);
-									});
-									break;
-							}
-						} else {
-							msg.payload = await request;
-							send(msg);
-						}
+                case "forEach":
+                  await request.forEach(function (payload) {
+                    msg.payload = payload;
+                    send(msg);
+                  });
+                  break;
+              }
+            } else {
+              msg.payload = await request;
+              send(msg);
+            }
 
-						// display node status
-						node.n.counter.success++;
-						node.status({
-							fill: "green",
-							shape: "dot",
-							text: `success ${node.n.counter.success}, error ${node.n.counter.error}`,
-						});
-					} catch (err) {
-						node.n.counter.error++;
-						node.status({
-							fill: "red",
-							shape: "ring",
-							text: "operation error",
-						});
-						node.error(err);
-					}
+            // display node status
+            node.n.counter.success++;
+            node.status({
+              fill: "green",
+              shape: "dot",
+              text: `success ${node.n.counter.success}, error ${node.n.counter.error}`,
+            });
+          } catch (err) {
+            node.n.counter.error++;
+            node.status({
+              fill: "red",
+              shape: "ring",
+              text: "operation error",
+            });
+            node.error(err);
+          }
 
-					if (done) {
-						done();
-					}
-				});
-				// end of node input
-			} catch (err) {
-				counter = 0;
-				node.status({ fill: "red", shape: "ring", text: "error" });
-				node.error(err);
-			}
-		};
+          if (done) {
+            done();
+          }
+        });
+        // end of node input
+      } catch (err) {
+        counter = 0;
+        node.status({ fill: "red", shape: "ring", text: "error" });
+        node.error(err);
+      }
+    };
 
-		if (node.n.client) {
-			connect();
-		} else {
-			node.status({ fill: "red", shape: "ring", text: "error" });
-			node.error("Missing node configuration");
-		}
+    if (node.n.client) {
+      connect();
+    } else {
+      node.status({ fill: "red", shape: "ring", text: "error" });
+      node.error("Missing node configuration");
+    }
 
-		node.on("close", function (removed, done) {
-			if (node.n.connection) {
-				node.n.connection.close();
-			}
-			done();
-		});
-	}
+    node.on("close", function (removed, done) {
+      if (node.n.connection) {
+        node.n.connection.close();
+      }
+      done();
+    });
+  }
 
-	RED.nodes.registerType("mongodb4", OperationNode);
+  RED.nodes.registerType("mongodb4", OperationNode);
 };
